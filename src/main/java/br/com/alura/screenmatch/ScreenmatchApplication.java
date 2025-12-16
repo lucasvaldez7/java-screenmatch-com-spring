9e9faf43036646ff684bd7484b1e75da2d4bfc4f
Lucas <lucass.valdez06@gmail.com>
2025-12-16 19:34:25 -0300

    Criando a aplicação ScreenMatch com Spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=divertidamente&apikey=416222bf");
+		System.out.println(json);
+	}
+
+}
