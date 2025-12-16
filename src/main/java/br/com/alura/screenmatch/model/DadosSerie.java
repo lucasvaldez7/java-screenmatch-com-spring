96a1f2fa2549f0b07714d4cfd0c6f2ac0da9097d
Lucas <lucass.valdez06@gmail.com>
2025-12-16 20:47:55 -0300

    feat/desserializando-dados

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,7 @@
+package br.com.alura.screenmatch.model;
+
+public record DadosSerie(String Title,
+                            String imdbRating, 
+                            Integer totalSeasons) {
+    
+}
