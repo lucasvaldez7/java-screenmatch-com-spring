96dcf3310cc3dc9985ba17692d0674f4713eedf9
Lucas <lucass.valdez06@gmail.com>
2025-12-16 21:21:41 -0300

    feat/consumo-de-API-e-conversao-de-dados-no-principal

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverterDados;
+
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,8 +20,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=divertidamente&apikey=416222bf");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=stranger+things&apikey=416222bf");
 		System.out.println(json);
+		ConverterDados conversor = new ConverterDados();
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
 	}
 
 }
